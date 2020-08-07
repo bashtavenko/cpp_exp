85117c1a27f7d91109d7ca91b5d9a173bb24801e
bashtavenko <stanbpublic@live.com>
2020-08-07 09:12:08 -0400

    Leave only language-specific workspace

--- hello/hello.cc
+++ hello/hello.cc
@@ -2,8 +2,11 @@
 
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "base/statusor.h"
 
 namespace hello {
+
+    differential_privacy::base::StatusOr<int> s;
     std::string Greet(absl::string_view person) {
         return absl::StrCat("Hello ", person);
     }
