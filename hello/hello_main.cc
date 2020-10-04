e25a5b5510c108d3224d5182f5b646074bdf71cf
bashtavenko <stanbpublic@live.com>
2020-10-04 12:00:19 -0400

    Glog

--- hello/hello_main.cc
+++ hello/hello_main.cc
@@ -1,7 +1,9 @@
 #include "hello.h"
-#include <iostream>
+#include <glog/logging.h>
 
 int main(int argc, const char** argv) {
-    std::cout << hello::Greet(argc < 2 ? "world" : argv[1]) << std::endl;
+    google::InitGoogleLogging(argv[0]);
+    CHECK_NE(2, 1) << ": The world must be ending!";
+    LOG(INFO) << hello::Greet(argc < 2 ? "world" : argv[1]) << std::endl;
     return 0;
 }
