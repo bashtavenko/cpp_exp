6811174dc8b07f30cc39e4292d94aabcc32682d2
bashtavenko <stanbpublic@live.com>
2022-06-12 14:37:35 +0000

    Testdata added

--- opencv_hello/mat_test.cc
+++ opencv_hello/mat_test.cc
@@ -1,14 +1,29 @@
 #include "include/gtest/gtest.h"
 #include "opencv/opencv_cmake/include/opencv2/core.hpp"
+#include "opencv/opencv_cmake/include/opencv2/imgcodecs.hpp"
+#include "tools/cpp/runfiles/runfiles.h"
 
 namespace opencv_hello {
 namespace {
+using bazel::tools::cpp::runfiles::Runfiles;
 using cv::Mat;
 using cv::Scalar;
+using cv::imread;
 
 TEST(Mat, Works) {
+  Runfiles* runfiles = Runfiles::CreateForTest();
+  ASSERT_NE(runfiles, nullptr);
+
+  // Read from file
+  Mat mat1 = imread(runfiles->Rlocation("opencv_hello/testdata/starry_night.jpg"));
+  ASSERT_FALSE(mat1.empty());
+  ASSERT_EQ(mat1.dims, 2);
+  ASSERT_EQ(mat1.rows, 600);
+  ASSERT_EQ(mat1.cols, 752);
+
+  // Create
   Mat mat(2, 2, CV_8UC1, Scalar(0, 0, 255));
-  EXPECT_EQ("Hello", "Hello");
+  ASSERT_EQ(mat.dims, 2);
 }
 } // namespace
 } //  namespace opencv_hello
