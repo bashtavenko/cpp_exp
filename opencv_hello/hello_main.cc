8fe8bcf9f607ca40960d21b373ce9b14d08f1f1f
bashtavenko <stanbpublic@live.com>
2022-11-23 19:24:39 +0000

    Misc updates

--- opencv_hello/hello_main.cc
+++ opencv_hello/hello_main.cc
@@ -1,9 +1,11 @@
 #include "misc/misc.h"
 #include "convolution/filters.h"
 #include "transformations/transformations.h"
+#include "histograms/histograms.h"
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include "absl/status/status.h"
+#include "fft/fft.h"
 
 int main(int argc, char** argv) {
 
@@ -12,7 +14,9 @@ int main(int argc, char** argv) {
 
 //  absl::Status status = hello::misc::ShowVideoCanny();
 //  absl::Status status = hello::convolution::AdaptiveThreshold();
-  absl::Status status = hello::transformations::PerspectiveTransform();
+//  absl::Status status = hello::transformations::PerspectiveTransform();
+//  absl::Status status = hello::fft::FastConv();
+  absl::Status status = hello::histograms::Match();
   LOG(INFO) << status.code();
   return status.ok() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
