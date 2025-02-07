ca4c4b9613120fa4bd3d875313dc42d29f8d3f5b
bashtavenko <stanbpublic@live.com>
2025-02-07 09:05:11 -0500

    2025 updates

--- hello/hello_main.cc
+++ hello/hello_main.cc
@@ -1,22 +1,22 @@
-#include "hello.h"
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <glog/stl_logging.h>
+#include "hello.h"
 
 int main(int argc, char** argv) {
-    google::InitGoogleLogging(argv[0]);
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    CHECK_NE(1, 2) << ": The world must be ending!";
+  CHECK_NE(1, 2) << ": The world must be ending!";
 
-    // Requires --logtostderr=1 flag
-    LOG(INFO) << hello::Greet(argc < 2 ? "world" : argv[1]) << std::endl;
+  // Requires --logtostderr=1 flag
+  LOG(INFO) << hello::Greet(argc < 2 ? "world" : argv[1]) << std::endl;
 
-    std::vector<int> x;
-    x.push_back(1);
-    x.push_back(2);
-    x.push_back(3);
-    LOG(INFO) << "ABC, it's easy as " << x;
+  std::vector<int> x;
+  x.push_back(1);
+  x.push_back(2);
+  x.push_back(3);
+  LOG(INFO) << "ABC, it's easy as " << x;
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
--- hello/hello_test.cc
+++ hello/hello_test.cc
@@ -3,9 +3,9 @@
 #include "gtest/gtest.h"
 
 namespace hello {
-    namespace {
-        TEST(Greeter, Works) {
-        EXPECT_EQ("Hello tester", Greet("tester"));
-    }
+namespace {
+
+TEST(Greeter, Works) { EXPECT_EQ("Hello tester", Greet("tester")); }
+
 }  // namespace
 }  // namespace hello
